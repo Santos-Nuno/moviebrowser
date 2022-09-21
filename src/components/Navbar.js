e554b48ad75cc191354d683f4581b180ff1c4224
Santos-Nuno <nunosantos11@gmail.com>
2022-09-21 15:28:06 +0100

    My Changes

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -11,7 +11,7 @@ const Navbar = ({ searchText, setSearchText }) => {
   return (
     <nav className="navbar navbar-expand-lg bg-light">
       <div className="container-fluid">
-        <Link className="navbar-brand" to="/">
+        <Link className="navbar-brand" to="/moviebrowser">
           Movie Browser
         </Link>
         <button
@@ -28,7 +28,7 @@ const Navbar = ({ searchText, setSearchText }) => {
         <div className="collapse navbar-collapse" id="navbarSupportedContent">
           <ul className="navbar-nav me-auto mb-2 mb-lg-0">
             <li className="nav-item">
-              <Link className="nav-link active" aria-current="page" to="/">
+              <Link className="nav-link active" aria-current="page" to="/moviebrowser">
                 Home
               </Link>
             </li>
